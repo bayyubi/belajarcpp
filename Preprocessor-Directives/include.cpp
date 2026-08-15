599ed456aa3b5903374356c330db4e6404528da1
bayyubi <ubayrafif.js2@gmail.com>
2026-08-16 00:47:03 +0700

    Belajar C++

---
+++ Preprocessor-Directives/include.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //?pustaka untuk input & output.
+
+using namespace std; //?menggunakan namespace std agar tidak perlu menulis std:: sebelum cout dan endl.
+
+int main() {
+    cout << "Hai!" << endl;
+    return 0;
+}
