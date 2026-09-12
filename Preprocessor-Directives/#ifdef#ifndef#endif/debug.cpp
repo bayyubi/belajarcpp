4697be7f806bbefa6568cea13442fc3f1a064356
bayyubi <ubayrafif.js2@gmail.com>
2026-09-13 03:23:01 +0700

    Belajar C++

--- Preprocessor-Directives/#ifdef#ifndef#endif/debug.cpp
+++ Preprocessor-Directives/#ifdef#ifndef#endif/debug.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    #ifdef DEBUG
+    #ifdef DEBUG //? Memeriksa apakah DEBUG telah didefinisikan sebelumnya menggunakan #define
         cout <<"Mode Debug Aktif!"<< endl;
     #endif
 
